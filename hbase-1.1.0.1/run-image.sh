e899dcdba35acd3d6749e51661bad9276f66303e
extrsi <extrsi@users.noreply.github.com>
2015-09-24 13:22:22 +0200

    Update run-image.sh

--- hbase-1.1.0.1/run-image.sh
+++ hbase-1.1.0.1/run-image.sh
@@ -1,1 +1,1 @@
-docker run -ti --net=host -p 2181:2181 -p 60010:60010 -p 60000:60000 -p 60020:60020 -p 60030:60030 --host 192.168.99.100 extrsi/hbase
+docker run -ti --net=host -p 2181:2181 -p 60010:60010 -p 60000:60000 -p 60020:60020 -p 60030:60030 extrsi/hbase-for-kitematic
